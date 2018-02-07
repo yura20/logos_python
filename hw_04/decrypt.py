ab83d432852d896786a9485b1caf33f85f3cfae4
Yuriy Kasper <31483577+yura20@users.noreply.github.com>
2018-02-07 16:26:51 +0200

    hw_04

--- hw_04/decrypt.py
+++ hw_04/decrypt.py
@@ -3,18 +3,15 @@ text = input('enter encrypt text: ')
 text = text.replace(' ', '')
 l = len(text)-len(text)%5
 text = text[0:l]
-print(text)
 ab_text = ""
 for i in text:
     if i.islower():
         ab_text+='a'
     else:
         ab_text+='b'
-print(ab_text)
 
 ab_list = list(ab_text)
 
-print(ab_list)
 
 ab = []
 
@@ -24,20 +21,13 @@ for i in ab_list:
         list_01.append("0")
     else:
         list_01.append("1")
-print(list_01)
 i=0      
 while i<l:
     ab.append(int(list_01[i]+list_01[i+1]+list_01[i+2]+list_01[i+3]+list_01[i+4], 2)+97)
     
     i+=5
-    
-print(ab)
 decrypt=""
 for i in ab:
     decrypt+=chr(i)
     
-print("decrypt word = "decrypt)
-
-    
-
-
+print("decrypt word = ",decrypt)
